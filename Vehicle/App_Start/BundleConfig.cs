e17a36b25f52e98864667a59a83312cbe06086bf
olivermitev <mitev_oliver@yahoo.com>
2020-03-09 11:13:39 +0100

    admin dashboard

--- Vehicle/App_Start/BundleConfig.cs
+++ Vehicle/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Vehicle
 {
@@ -13,6 +12,10 @@ namespace Vehicle
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
